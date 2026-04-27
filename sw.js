d55dcd61fee2c9353544073599f1528ec0f8d916
tuanzia7-commits <tuanzia7@gmail.com>
2026-04-28 07:17:51 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+// 这是一个基础的运行脚本，让浏览器认为这是一个 APP
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('PWA 脚本已激活');
+});
+
+self.addEventListener('fetch', (event) => {
+  // 保持联网正常运行
+  event.respondWith(fetch(event.request));
+});
